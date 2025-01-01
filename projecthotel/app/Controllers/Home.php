1b0f71ab953452788263f8e094b6856be01a7eec
FsRitsu <rafimnajmuddin@gmail.com>
2025-01-01 15:07:50 +0700

    Initial commit

---
+++ projecthotel/app/Controllers/Home.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+    public function index()
+    {
+        // return view(name: 'layouts/navbar').
+        // view('home/index').
+        // view(name: 'layouts/footer');
+
+        echo "tas";
+    }
+
+}
